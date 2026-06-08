2c62022cdd57d326928347a70f3796369c8a66e6
tcfplayz <chau123ka89chun10p@gmail.com>
2026-06-09 04:45:33 +0800

    v3.3.6 - yup im not touching you for another day

--- src/main/java/net/enchantoutline/mixin/BlockEntityWithoutLevelRendererMixin.java
+++ src/main/java/net/enchantoutline/mixin/BlockEntityWithoutLevelRendererMixin.java
@@ -11,7 +11,6 @@ import net.minecraft.client.model.TridentModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -48,13 +47,6 @@ public class BlockEntityWithoutLevelRendererMixin {
             poseStack.pushPose();
             poseStack.mulPose(com.mojang.math.Axis.XP.rotationDegrees(180f));
             poseStack.mulPose(com.mojang.math.Axis.YP.rotationDegrees(180f));
-        }else {
-            String namespace = BuiltInRegistries.ITEM.getKey(stack.getItem()).getNamespace();
-            if (namespace.equals("cataclysm") || namespace.equals("silentgear")) {
-                // If the mod uses standard Geo/Baked models inside their BEWLR wrapper,
-                // you can extract their root model parts or custom vertex buffers here.
-                // For now, this safely separates them from breaking vanilla passes.
-            }
         }
 
         // TODO: cataclysm has the custom renderer here (class CMItemstackRenderer extends BlockEntityWithoutLevelRenderer), probably support here
