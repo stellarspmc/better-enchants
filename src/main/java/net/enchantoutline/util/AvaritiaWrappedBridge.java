88715f49f0127ef2e78617e41aaf873cead00231
tcfplayz <chau123ka89chun10p@gmail.com>
2026-06-09 04:50:50 +0800

    v3.3.6 - refactor

--- src/main/java/net/enchantoutline/util/AvaritiaWrappedBridge.java
+++
@@ -1,9 +0,0 @@
-package net.enchantoutline.util;
-
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.world.item.ItemStack;
-
-public interface AvaritiaWrappedBridge {
-    void enchant_outline$bridgeRenderWrapped(ItemStack stack, PoseStack matrixStack, MultiBufferSource buffer, int combinedLight, int combinedOverlay, boolean allowRender);
-}
